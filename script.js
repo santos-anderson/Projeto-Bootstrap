faefa651108dc7736c66099b91994e534ac59f48
Anderson Alves Santos <santos.anders@gmail.com>
2024-12-11 23:45:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,4 @@ function showAlert(gameName) {
     alert('Você clicou em ' + gameName + '!');
 function showAlert(gameName) {
     alert('Você clicou em ' + gameName + '!');
->>>>>>> c94924f80550a30cd9116d9617c4fc84a9b30819
 }
